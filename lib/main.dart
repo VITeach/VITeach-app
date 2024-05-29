b1d3a11998aaee74c4a751d4d83deb2763f51507
Aditya Pandey <adityapandey2395@gmail.com>
2024-05-29 21:54:07 +0530

    Modified utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:viteach_learning_app/utils/theme/theme.dart';
+import 'app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: VAppTheme.lightTheme,
-      darkTheme: VAppTheme.darkTheme,
-    );
-  }
 }
